18dcb7252c7dc8326519788cb0cd38a2ff319ee9
ozgurmtunca <ozgurmtunca@gmail.com>
2021-09-07 22:50:29 +0300

    header continue

--- template-parts/headers/layout-1.php
+++ template-parts/headers/layout-1.php
@@ -1,13 +1,18 @@
-<div class="container-fluid sticky-top" style="background: rgba(0,0,0,0.7);">
+<div id="topbar" class="topbar header-container">
     <div class="pardus-container">
-        <div class="row">
-        <?php if ( intval(pardus_get_option( 'header_promo' ) ) ) : ?>
-            <div class="col-xs-12 col-sm-12 col-md-5 hidden-xs hidden-sm text-center promo-text">
-                <h1 class="school-logo-text float-left">
-                    <a href="<?php  echo site_url(); ?>"><strong>Pardus</strong> The Cat</a>
-                </h1>
-            </div>
-            <?php endif; ?>
+      <div class="row topbar-row">
+    <?php if ( intval(pardus_get_option( 'header_promo' ) ) ) : ?>
+        <div class="topbar-left topbar-sidebar col-xs-12 col-sm-12 col-md-5 hidden-xs hidden-sm">
+            <h1 class="pardus-logo-text float-left">
+                <a href="<?php  echo site_url(); ?>"><strong>Pardus</strong> The Cat</a>
+            </h1>
         </div>
+        <div class="topbar-right topbar-sidebar col-xs-12 col-sm-12 col-md-7 hidden-xs hidden-sm">
+            <h1 class="pardus-logo-text float-left">
+                <a href="<?php  echo site_url(); ?>"><strong>Pardus</strong> The Cat</a>
+            </h1>
+        </div>  
+    <?php endif; ?>
+    </div>
     </div>
 </div>
